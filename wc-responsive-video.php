0b38844cd835331dd84a67f59fc2032258232b38
cbaldelomar <cbaldelomar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-18 19:06:44 +0000

    version 1.9
    git-svn-id: https://plugins.svn.wordpress.org/wc-responsive-video/trunk@1093617 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wc-responsive-video.php
+++ wc-responsive-video.php
@@ -5,11 +5,16 @@ Plugin URI: http://webplantmedia.com/starter-themes/wordpresscanvas/features/plu
 Description: Simple responsive video plugin. Automatically determine aspect ratio. CSS only. No Javascript.
 Author: Chris Baldelomar
 Author URI: http://webplantmedia.com/
-Version: 1.8
+Version: 1.9
 License: GPLv2 or later
 */
 
-define( 'WC_RESPONSIVE_VIDEO_VERSION', '1.8' );
+define( 'WC_RESPONSIVE_VIDEO_VERSION', '1.9' );
+
+// Should only work on the front end
+if ( is_admin() ) {
+	return;
+}
 
 require_once( dirname(__FILE__) . '/includes/functions.php' ); // Adds basic filters and actions
 require_once( dirname(__FILE__) . '/includes/scripts.php' ); // Adds plugin JS and CSS
